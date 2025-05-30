450e86389addade64c027f4bafe558e123bfcdd4
RonnieRods <ronrode7@gmail.com>
2025-05-30 14:35:26 -0500

    Display final scores within the console

--- script.js
+++ script.js
@@ -1,24 +1,66 @@
-let computer = '';
 let human = '';
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
-        console.log((computer = 'Rock'));
-    } else if (computerChoice === 1) {
-        console.log((computer = 'Paper'));
+    let computerPicks = Math.floor(Math.random() * 3);
+    if (computerPicks === 0) {
+        return 'Rock';
+    } else if (computerPicks === 1) {
+        return 'Paper';
     } else {
-        console.log((computer = 'Scissors'));
+        return 'Scissors';
     }
 }
 
 function getHumanChoice() {
     human = prompt('Please enter Rock, Paper or Scissors:');
-    human = human.charAt(0).toUpperCase() + human.slice(1, human.length);
+    human = human.charAt(0).toUpperCase() + human.slice(1, human.length).toLowerCase();
     if (human === "Rock" || human === "Paper" || human === "Scissors") {
-        console.log(human);
+        return human;
     } else {
         console.log("Try typing rock, paper or scissors!");
-        getHumanChoice();
+        human = prompt('Please enter Rock, Paper or Scissors:');
+        human = human.charAt(0).toUpperCase() + human.slice(1, human.length).toLowerCase();
+        return human;
     }
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (
+        humanChoice === "Rock" && computerChoice === "Scissors" ||
+        humanChoice === "Paper" && computerChoice === "Rock" ||
+        humanChoice === "Scissors" && computerChoice === "Paper"
+    ) {
+        ++humanScore;
+        console.log(`You win this round! ${humanChoice} beats ${computerChoice}! Human: ${humanScore} Computer: ${computerScore}`);
+    } else if (
+        humanChoice === "Rock" && computerChoice === "Paper" ||
+        humanChoice === "Paper" && computerChoice === "Scissors" ||
+        humanChoice === "Scissors" && computerChoice === "Rock"
+    ) {
+        ++computerScore;
+        console.log(`You lost this round! ${computerChoice} beats ${humanChoice}!  Human: ${humanScore} Computer: ${computerScore}`);
+
+    } else if (
+        humanChoice === computerChoice ||
+        humanChoice === computerChoice ||
+        humanChoice === computerChoice
+    ) {
+        console.log(`It's a tie! You both chose ${humanChoice}!`);
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+
+if (humanScore > computerScore) {
+    console.log(`Congratulations! You beat the computer! Final Scores ---> Human: ${humanScore} | Computer: ${computerScore}`)
+} else if (computerScore > humanScore) {
+    console.log(`How unfortunate.... you lost to the computer! Final Scores ---> Human: ${humanScore} | Computer: ${computerScore}`);
+} else if (humanScore === computerScore) {
+    console.log(`What are the odds?! You both tied with ${humanScore} points each!`);
 }
